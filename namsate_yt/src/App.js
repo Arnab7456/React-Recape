99f3ae7e598875d5b712631bf852491c8ef4690f
arnav <arnabdas2753@gmail.com>
2023-12-31 20:12:05 +0530

    new commit

---
+++ namsate_yt/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import Body from './components/Body';
+import Head from './components/Head';
+
+function App() {
+  return (
+   <div>
+    
+    <Head />
+    <Body />
+   </div>
+  );
+}
+
+export default App;
+
+/*
+  Head
+  Body
+    Sidebar
+      menuItems
+    mainContainer
+      ButtonList
+      VideoContainer
+        VideoCard
+*/
---
+++ namsate_yt/src/components/Sidebar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='p-5 w-48 shadow-lg'>
+      <h1 className='font-bold'>Subscription</h1>
+      <ul>
+        
+        <li>Music</li>
+        <li>Gaming</li>
+        <li>Sports</li>
+        <li>Movies</li>
+        </ul> 
+        <h1 className='font-bold'>Watch Later</h1>
+        <ul>
+        <li>Music</li>
+        <li>Gaming</li>
+        <li>Sports</li>
+        <li>Movies</li>
+        </ul>
+         
+    </div>
+  )
+}
+
+export default Sidebar
